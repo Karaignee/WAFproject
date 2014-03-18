6e7f7782c359cd00526a7dfdd452294f8c62236a
Karaignee <karen_rooney@hotmail.com>
2014-03-18 16:04:11 +0000

    changes made pre submission to NCI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "=set up user groups or everything will break="
+puts
+if !Rails.env.test?
+
+  print "01 UserGroup..."
+  UserGroup.delete_all
+  UserGroup.create!(id: 1, name: "Admin", description: "Can do anything on the system",
+                    is_an_admin: true, is_a_member: true)
+  UserGroup.create!(id: 2, name: "Charity", description: "Charity or NGO, regular access and can create projects",
+                    is_an_admin: false, is_a_member: true)
+  UserGroup.create!(id: 2, name: "Volunteer", description: "Volunteer with regular access",
+                    is_an_admin: false, is_a_member: true)
+  puts "ok"
+
+  puts; puts "--- end ---"
+
+end
+
